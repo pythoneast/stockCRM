7c8a63ce86e6fff914c1fab4f0f34b59b8a639dc
pythoneast <mrpalitaev@gmail.com>
2018-11-30 02:49:07 +0600

    Dockerized Django app

---
+++ README.md
@@ -0,0 +1,2 @@
+The stock CRM
+
---
+++ web/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3.6 manage.py migrate
+python3.6 manage.py collectstatic --noinput
+echo Starting Gunicorn
+exec /usr/local/bin/gunicorn stockCRM.wsgi:application -w 2 -b :8000
+
