fc658079257c1372389c6e9ed3363a46a0a8ae1b
Ali <mrpalitaev@gmail.com>
2018-12-02 22:35:39 +0600

    Added depends_on option in production.yml

--- web/stockCRM/settings.py
+++ web/stockCRM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*av(um-ebdr4e=nnt=3glay_yurnuvqo!t(*siny0by%yn@uz9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
